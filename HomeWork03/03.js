191cec8bad7418b51b336e7d55e84116014cc9f1
Abdelrady Tantawy <abdelrady@gmail.com>
2019-05-30 09:57:46 -0500

    Add observations for lab03

--- HomeWork03/03.js
+++ HomeWork03/03.js
@@ -31,3 +31,11 @@ subject.subscribe(ctx => {
 app = http.createServer((req, res) => {
     subject.next({ req, res });
 }).listen(config.port, ()=> console.log(`Successful Listen on port ${config.port}`));
+
+/*
+Observations:
+1- Sync: will fill memory fast with the file size (~400 MB) and no more requests can be fulfilled untill first one is done
+2- Async: will fill memory fast with the file size (~400 MB) but requests can be accepted until I/O read operation is ready to handle old requests
+3- Stream: I found that memory won't exceed 18MB which will make server more efficient to handle lots of requests in a non-blocking way 
+
+*/
